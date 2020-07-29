3da35d08302887bdc382a0911a7649e8dd7aa35f
nanthu0123 <nanthu0123@gmail.com>
2020-07-29 22:28:45 +0530

    template created

--- tasks/views.py
+++ tasks/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+
 from .models import *
+from .forms import *
 
 
 def index(request):
-    tasks=Task.objects.all()
-    context={'tasks':tasks}
-    return render(request, 'tasks/list.html',context)
+    tasks = Task.objects.all()
+    form = TaskForm()
+    context = {'tasks': tasks, 'form': form}
+    return render(request, 'tasks/list.html', context)
