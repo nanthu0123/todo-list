66c4ac1f490ff396cb0fbba0e8eab69b247be448
nanthu0123 <nanthu0123@gmail.com>
2020-09-03 23:13:44 +0530

    include comments

--- tasks/models.py
+++ tasks/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+# crate the model for user fields
 class Task(models.Model):
     title=models.CharField(max_length=200)
     complete=models.BooleanField(default=False)
     created=models.DateTimeField(auto_now_add=True)
+
 
     def __str__(self):
         return self.title
